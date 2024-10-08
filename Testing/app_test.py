d80be1c760cd59fb60f70bb80574b1bdad55da19
Rhyzeth <srtsteph40@icloud.com>
2024-10-08 16:52:03 -0400

    test file creation and initial code #8

---
+++ Testing/app_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestApp(unittest.TestCase):
+    
+    def __init__(self):
+        self.null = None
+
+    def Function1Test():
+        return None
+    
+if __name__ == '__main__':
+    unittest.main()
