f83e68528dd0930f9415f23f85070ffacbadbcba
Rhyzeth <srtsteph40@icloud.com>
2024-10-10 23:51:38 -0400

    (:

--- config.py
+++ config.py
@@ -1,19 +1,20 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+db_path = os.path.join(basedir, 'db', 'dnd_database.db')
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-secret-key')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-default-secret-key'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///db/dnd_database.db'
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{db_path}'
 
 class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{db_path}'
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///db/dnd_database.db'
+
 config = {
     'development': DevelopmentConfig,
     'production': ProductionConfig,
