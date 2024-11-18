c5de377b8f7afc927315cde4863d12d1bb2d7219
Rhyzeth <srtsteph40@icloud.com>
2024-11-18 09:28:57 -0500

    may temp changes and files, testing pug out by using existing html files i created

--- myExpressApp/routes/index.js
+++ myExpressApp/routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/home', (req, res) => {
+  res.render('home', {title: 'Home'})
+});
+
+router.get('/news', (req, res) => {
+  res.render('news', { title: 'News' });
+});
+
+router.get('/matchups', (req, res) => {
+  res.render('matchups', { title: 'Matchups' });
 });
 
 module.exports = router;
