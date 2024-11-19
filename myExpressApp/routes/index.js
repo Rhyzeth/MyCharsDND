8ce80110d9d6486e3453f76ea19954a19d56e3af
Rhyzeth <srtsteph40@icloud.com>
2024-11-19 18:59:51 -0500

    removal of items relating to other project, testing and experience gain was successful, continued creation of this project can now resume

--- myExpressApp/routes/index.js
+++ myExpressApp/routes/index.js
@@ -3,31 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('home', { title: 'Home' });
 });
 
-router.get('/home', (req, res) => {
-  res.render('home', {title: 'LandingPage'})
-});
-
-router.get('/news', (req, res) => {
-  res.render('news', { title: 'News' });
-});
-
-router.get('/matchups', (req, res) => {
-  res.render('matchups', { title: 'Matchups' });
-});
-
-router.get('/AFCTeams', (req, res) => {
-  res.render('AFCTeams', { title: 'AFCTeams' });
-});
-
-router.get('/NFCTeams', (req, res) => {
-  res.render('NFCTeams', { title: 'NFCTeams' });
-});
-
-router.get('/PowerRankings', (req, res) => {
-  res.render('PowerRankings', { title: 'PowerRankings' });
-});
 
 module.exports = router;
